c748737f65bd268b82b243d62e055cbecff90b6c
Eddie Antonio Santos <easantos@ualberta.ca>
2015-12-24 14:21:05 -0700

    Add the world's most pointless REPL.

--- brainmuk.c
+++ brainmuk.c
@@ -8,9 +8,11 @@
 #include <bf_compile.h>
 #include <bf_slurp.h>
 
+#define REPL_LINE_LENGTH 1024
+
 static char* program_name = NULL;
-static void run_program(program_t program, bf_options *options);
 static void run_file(bf_options *options);
+static void repl(bf_options *options);
 
 int main(int argc, char *argv[]) {
     program_name = argv[0];
@@ -18,12 +20,93 @@ int main(int argc, char *argv[]) {
 
     /* Check if a file has been provided. */
     if (options.filename == NULL) {
-        fprintf(stderr, "%s: Must provide one filename\n", argv[0]);
-        usage_error(program_name);
+        repl(&options);
+    } else {
+        run_file(&options);
     }
-    run_file(&options);
 
     return 0;
+}
+
+
+static uint8_t* create_universe(bf_options *options) {
+    uint8_t* universe =
+        calloc(options->minimum_universe_size, sizeof(uint8_t));
+    if (universe == NULL) {
+        fprintf(stderr, "%s: could not create universe (%lu bytes): ",
+                program_name, options->minimum_universe_size);
+        perror(NULL);
+        exit(-1);
+    }
+
+    return universe;
+}
+
+static struct bf_runtime_context normal_context(uint8_t *universe) {
+    return ((struct bf_runtime_context) {
+            .universe = universe,
+            .output_byte = bf_runtime_output_byte,
+            .input_byte = bf_runtime_input_byte
+    });
+}
+
+static void prompt(const char *herp) {
+    printf("%s ", herp);
+    fflush(stdout);
+}
+
+
+static void repl(bf_options *options) {
+    char line[REPL_LINE_LENGTH];
+
+    /* Warn if stdin doesn't appear to be a terminal. */
+    if (!isatty(fileno(stdin))) {
+        fprintf(stderr,
+                "%s: warning: input is not from a terminal\n",
+                program_name);
+    }
+
+    /* Prepare universe and executable space. */
+    const size_t exec_mem_size = sysconf(_SC_PAGESIZE);
+    uint8_t *universe = create_universe(options);
+    uint8_t *exec_mem = allocate_executable_space(exec_mem_size);
+
+    do {
+        prompt("#%@!>");
+
+        /* Read. */
+        if (fgets(line, REPL_LINE_LENGTH, stdin) == NULL) {
+            break;
+        }
+
+        /* Eval. */
+        bf_compile_result result = bf_compile(line, exec_mem);
+
+        if (result.status != BF_COMPILE_SUCCESS) {
+            fprintf(stderr, "compile error (check brackets?)\n");
+            continue;
+        }
+
+        /* Run! */
+        result.program(normal_context(universe));
+
+        /* (The program should print stuff itself... */
+    } while (!feof(stdin));
+
+    free(universe);
+}
+
+static void run_program(program_t program, bf_options *options) {
+    /* Allocate the ENTIRE UNIVERSE and run. */
+    uint8_t *universe = calloc(options->minimum_universe_size, sizeof(uint8_t));
+
+    program((struct bf_runtime_context) {
+            .universe = universe,
+            .output_byte = bf_runtime_output_byte,
+            .input_byte = bf_runtime_input_byte
+    });
+
+    free(universe);
 }
 
 static void run_file(bf_options *options) {
@@ -47,31 +130,11 @@ static void run_file(bf_options *options) {
 
     if (compilation.status == BF_COMPILE_SUCCESS) {
         run_program(compilation.program, options);
-        free_executable_space(exec_mem, exec_mem_size);
     } else {
-        free_executable_space(exec_mem, exec_mem_size);
         fprintf(stderr, "%s: %s: compilation failed!\n",
                 program_name, options->filename);
-        exit(compilation.status);
-    }
-}
-
-static void run_program(program_t program, bf_options *options) {
-    /* Allocate the ENTIRE UNIVERSE and run. */
-    uint8_t *universe = calloc(options->minimum_universe_size, sizeof(uint8_t));
-
-    if (universe == NULL) {
-        fprintf(stderr, "%s: could not create universe (%lu bytes): ",
-                program_name, options->minimum_universe_size);
-        perror(NULL);
-        exit(-1);
     }
 
-    program((struct bf_runtime_context) {
-            .universe = universe,
-            .output_byte = bf_runtime_output_byte,
-            .input_byte = bf_runtime_input_byte
-    });
-
-    free(universe);
+    free_executable_space(exec_mem, exec_mem_size);
+    exit(compilation.status);
 }
